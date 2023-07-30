a1fef09ef6bcd21af5f1fe666d8189589634662f
Purvesh Rathod <purvesh62@gmail.com>
2023-07-30 00:03:36 -0300

    auth and stripe CRUD added

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-lg bg-cyan-500">Hello Next 13</h1>
+    </main>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true
-  }
-};
+  experimental: {
+    appDir: true,
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
